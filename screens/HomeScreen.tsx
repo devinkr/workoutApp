cd17d373c6d7f9ee46af9ff1a6173b6ef1171bbc
Devin Raleigh <devinkr@gmail.com>
2023-04-21 16:22:24 -0600

    Workout data and add custom font

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
+import data from '../data.json';
+import {Workout} from '../types/data';
+import WorkoutItem from '../components/WorkoutItem';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>I am a home screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>New Workouts</Text>
+      <FlatList
+        data={data as Array<Workout>}
+        renderItem={WorkoutItem}
+        keyExtractor={item => item.slug}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  header: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontFamily: 'Montserrat-Bold',
+  },
+});
